96b6ad13a21447bc0a6048dd09f2b8c449d9d3f1
Vasyl_Hulevych <vasiliy.mysiuk@gmail.com>
2019-04-13 21:26:12 +0300

    ElementSearcher class fixes: 1.Improved code style according Java code convention 2.Changed output when element is not in array

--- src/Task5/ElementSearcher.java
+++ src/Task5/ElementSearcher.java
@@ -3,18 +3,18 @@ package Task5;
 public class ElementSearcher {
     public static void main(String[] args) {
         int[] array = {158,207,392,62,315,434,487,268,405,241};
-        searchElement(array, 207); //type element to search here
+        searchElement(array, 434); //type element to search here
     }
-    public static void searchElement(int[] array, int el){
+    public static void searchElement(int[] array, int el) {
         int counter = 0;
-        for(int i = 0; i < array.length; i++){
-            if (array[i] == el){
+        for(int i = 0; i < array.length; i++) {
+            if (array[i] == el) {
                 System.out.println(el + " found under " + i + " index.");
                 counter++;
             }
         }
-        if (counter == 0){
-            System.out.println("No such element(" + el + ") in array");
+        if (counter == 0) {
+            System.out.println( el + " is not in array");
         }
     }
 }
