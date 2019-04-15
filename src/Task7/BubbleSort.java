ea5e5e0a61bd2083d79bc7c971f42bc4c000ac6a
Vasyl_Hulevych <vasiliy.mysiuk@gmail.com>
2019-04-15 23:57:12 +0300

    Improved code style according to Java code convention

--- src/Task7/BubbleSort.java
+++ src/Task7/BubbleSort.java
@@ -14,28 +14,26 @@ public class BubbleSort {
             System.out.print(array[i] + " ");
     }
 
-    public static int[] sortAscending(int[] array){
-        for (int i = 0; i < ( array.length - 1 ); i++) {
+    public static int[] sortAscending(int[] array) {
+        for (int i = 0; i < (array.length - 1); i++) {
             for (int j = 0; j < array.length - i - 1; j++) {
-                if (array[j] > array[j+1])
-                {
+                if (array[j] > array[j + 1]) {
                     int temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
             }
         }
         return array;
     }
 
-    public static int[] sortDescending(int[] array){
-        for (int i = 0; i < ( array.length - 1 ); i++) {
+    public static int[] sortDescending(int[] array) {
+        for (int i = 0; i < (array.length - 1); i++) {
             for (int j = 0; j < array.length - i - 1; j++) {
-                if (array[j] < array[j+1])
-                {
+                if (array[j] < array[j + 1]) {
                     int temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
             }
         }
